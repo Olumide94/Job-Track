95fea5d2f65bdba414bbe00f6d181755b53d894b
Olumide Lawal <95596252+Olumide94@users.noreply.github.com>
2026-02-25 22:49:58 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,31 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('jobtrack-cache').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
-  );
+const CACHE_NAME = 'jobtrack-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+// Activate service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+        ))
+    );
+});
+
+// Fetch files
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
